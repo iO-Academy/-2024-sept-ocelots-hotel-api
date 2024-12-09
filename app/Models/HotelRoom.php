0e4f45f5d715e65b903ddaea2356bb80f8207132
syed faheema <asyedfaheema@gmail.com>
2024-12-09 12:44:49 +0000

    Created Single room

--- app/Models/HotelRoom.php
+++ app/Models/HotelRoom.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class HotelRoom extends Model
 {
-    //
+    public function room_types(): BelongsTo
+    {
+        return $this->belongsTo(RoomType::class);
+    }
 }
--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class RoomType extends Model
 {
-    //
+  public function hotel_rooms(): HasMany
+  {
+      return $this->hasMany(HotelRoom::class);
+  }
+
 }
