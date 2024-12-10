08991b119ae5a368688a6af6083abf11cbb115c8
emsmarshall <mikemarshall1988@gmail.com>
2024-12-10 12:34:30 +0000

    updated testing

--- tests/Unit/DateServiceTest.php
+++ tests/Unit/DateServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Booking;
 use App\Services\DateService;
 use PHPUnit\Framework\TestCase;
 
@@ -14,20 +15,25 @@ class DateServiceTest extends TestCase
 
     }
 
-    public function testFutureDate()
+    public function testFutureDateGood()
     {
-        $inputGoodFutureDate = '2025-04-16';
-        $inputBadFutureDate = '2010-03-05';
-        $inputNotDate = 'gloob';
-
+        $inputGoodFutureDate = '2200-04-16';
         $actualGood = DateService::futureDate($inputGoodFutureDate);
         $expectedGood = true;
         $this->assertEquals($expectedGood, $actualGood);
+    }
 
+    public function testFutureDateBad()
+    {
+        $inputBadFutureDate = '2010-03-05';
         $actualBad = DateService::futureDate($inputBadFutureDate);
         $expectedBad = false;
         $this->assertEquals($expectedBad, $actualBad);
+    }
 
+    public function testFutureDateNotDate()
+    {
+        $inputNotDate = 'gloob';
         $actualNotDate = DateService::futureDate($inputNotDate);
         $expectedNotDate = false;
         $this->assertEquals($expectedNotDate, $actualNotDate);
@@ -36,6 +42,8 @@ class DateServiceTest extends TestCase
 
     public function testAvailableDate()
     {
+        $fakeConfirmedBooking = $this->createMock(Booking::class);
+//        $fakeConfirmedBooking->start=;
 
     }
 }
