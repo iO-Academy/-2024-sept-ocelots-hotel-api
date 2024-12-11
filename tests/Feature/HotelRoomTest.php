666185440b06d2b266bb4a1cd6a187758b3924e7
Dan Thomas <danthomas88@hotmail.com>
2024-12-11 10:09:29 +0000

    created factories for rooms and type

---
+++ tests/Feature/HotelRoomTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\HotelRoom;
+use App\Models\Type;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class HotelRoomTest extends TestCase
+{
+   use DatabaseMigrations;
+    public function test_getRooms_success(): void
+    {
+        HotelRoom::factory()->create();
+        Type::factory()->create();
+        $response = $this->getJson('api/rooms');
+
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message', 'data'])
+                    ->has('data', function (AssertableJson $data) {
+                        $data->hasAll(['id', 'name', 'image', 'min_capacity', 'max_capacity',
+                            'type_id', 'created_at', 'updated_at']);
+//                            ->whereAllType([
+//'id'=>in, 'name', 'image', 'min_capacity', 'max_capacity'])
+                    });
+            });
+
+    }
+}
