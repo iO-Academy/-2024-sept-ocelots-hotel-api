fa5560e898b78c538fb59403e8ed188d8e283ca5
Michael Day <michaelday1991@hotmail.co.uk>
2024-12-11 11:52:28 +0000

    Complete HotelRoomTest

--- tests/Feature/HotelRoomTest.php
+++ tests/Feature/HotelRoomTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertJson;
 
 class HotelRoomTest extends TestCase
 {
@@ -31,4 +32,29 @@ class HotelRoomTest extends TestCase
             });
 
     }
+    public function test_getSingleRoom_success(): void
+    {
+        $room = HotelRoom::factory()->create();
+        $response = $this->getJson("api/rooms/{$room->id}");
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) use ($room) {
+                $json->hasAll(['message', 'data'])
+                    ->has('data', function (AssertableJson $data) use ($room) {
+                        $data->hasAll(['id', 'name', 'image', 'min_capacity', 'max_capacity', 'type'])
+                        ->whereAllType([
+                            'id'=> 'integer', 'name'=>'string', 'image'=>'string', 'min_capacity' => 'integer', 'max_capacity'=>'integer']);
+                    });
+            });
+    }
+
+    public function test_getSingleRoom_error(): void
+    {
+        $response = $this->getJson("api/rooms/2");
+        $response->assertStatus(404)
+        ->assertJson(function (AssertableJson $json) {
+            $json->hasAll(['message']);
+                });
+    }
+
+
 }
