a3065a0215a3fe1b8335e4d57301b2cb60c0cd4a
Michael Day <michaelday1991@hotmail.co.uk>
2024-12-11 10:32:24 +0000

    Got HotelRoomTest working

--- tests/Feature/HotelRoomTest.php
+++ tests/Feature/HotelRoomTest.php
@@ -22,11 +22,11 @@ class HotelRoomTest extends TestCase
         $response->assertStatus(200)
             ->assertJson(function (AssertableJson $json) {
                 $json->hasAll(['message', 'data'])
-                    ->has('data', function (AssertableJson $data) {
+                    ->has('data',1, function (AssertableJson $data) {
                         $data->hasAll(['id', 'name', 'image', 'min_capacity', 'max_capacity',
-                            'type_id', 'created_at', 'updated_at']);
+                            'type']);
 //                            ->whereAllType([
-//'id'=>in, 'name', 'image', 'min_capacity', 'max_capacity'])
+//                            'id', 'name', 'image', 'min_capacity', 'max_capacity']);
                     });
             });
 
